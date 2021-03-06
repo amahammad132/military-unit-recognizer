c6a51438612a195c4ead63f86bc38f8f0f4b1166
Amaan Mahammad <60258136+amahammad132@users.noreply.github.com>
2021-03-05 21:47:39 -0800

    Improve formatting

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 from os import walk
 
+IMAGE_DIR = './image'
+TEMPLATE_IMAGE = IMAGE_DIR + '/' + 'template.png'
+
 
 # Turn a value from a range of [-1, 1] into a percentage string,
 # with colors indicating a pass or fail
@@ -11,14 +14,15 @@ def range_to_percent_pretty(value: float) -> str:
     return "%s%.2f%%%s" % (
         (Fore.GREEN if percentage_val >= 50 else Fore.RED),
         percentage_val,
-        Style.RESET_ALL)
+        Style.RESET_ALL
+    )
 
 
 # Check if a given image file name is similar to or different to the original
 def check_img(matching_image_name: str):
     img = cv2.imread(matching_image_name, 0)
 
-    print("Checking", matching_image_name)
+    print("\nChecking", matching_image_name)
 
     res = cv2.matchTemplate(template_img, img, cv2.TM_CCOEFF_NORMED)
 
@@ -28,22 +32,20 @@ def check_img(matching_image_name: str):
     corr2 = np.amax(res)
     print("corr2:", range_to_percent_pretty(corr2))
     if corr2 > 0.4:
-        print(f"{Fore.GREEN}PASS")
+        print(Fore.GREEN + "PASS")
     else:
-        print(f"{Fore.RED}FAIL")
-    print(Style.RESET_ALL + "\n")
+        print(Fore.RED + "FAIL")
+    print(Style.RESET_ALL, end='')
 
 
 if __name__ == '__main__':
-    IMAGE_DIR = './image'
-
     # Open the template image
-    template_img = cv2.imread('image/template.png', 0)
+    template_img = cv2.imread(TEMPLATE_IMAGE, 0)
 
     # A list of files in the image directory
     (_, _, image_list) = next(walk(IMAGE_DIR))
 
-    print(f"scanning through {image_list}")
+    print(f"Scanning through {image_list}")
 
     # Check the correlation of each image in the list
     for image in image_list:
